e2f5152f9b44efe71505507a94fb52b6ec3c0176
makhosi6 <ndondo330@gmail.com>
2021-05-15 20:47:10 +0200

    Updates

---
+++ precache-manifest.f9f868de873c367dfc64626ac07b9b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6ad3392ba0a1e3d97474429035f52c",
+    "url": "/muse/index.html"
+  },
+  {
+    "revision": "ab0d4b8ef2551d072d8b",
+    "url": "/muse/static/css/main.97d9c284.chunk.css"
+  },
+  {
+    "revision": "b64346d83159ee2330a8",
+    "url": "/muse/static/js/2.dcf2ab79.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/muse/static/js/2.dcf2ab79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0d4b8ef2551d072d8b",
+    "url": "/muse/static/js/main.6786b326.chunk.js"
+  },
+  {
+    "revision": "5e1db9ec79de3cbdff97",
+    "url": "/muse/static/js/runtime-main.92f8aded.js"
+  }
+]);
