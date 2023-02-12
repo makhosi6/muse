ceb618b88d3008ddb4faf4a3a02b3afc7ce69c8a
Makhosandile Ndondo <makhosi@Makhosandiles-MacBook-Air.local>
2023-02-13 00:58:07 +0200

    Mon 13 Feb 2023 00:58:07 SAST - use context, fetch data from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { MobileFriendly } from "@material-ui/icons";
 import { MobileWrapper } from "./components/layout/MobileWrapper";
@@ -10,34 +10,67 @@ import MoneyCharts from "./components/util/MoneyCharts";
 import { LoadMore } from "./components/articles/LoadMore";
 import { TopAppBar } from "./components/nav/TopAppBar";
 
+/**
+ * top trending article
+ */
+export const MainArticleContext = React.createContext({});
+
+/**
+ * List of all other articles
+ */
+export const ArticlesContext = React.createContext([]);
+
+/**
+ * Trending key words/phrases, extracted from google trends
+ */
+export const TrendsContext = React.createContext([]);
+
+/**
+ * Latest articles
+ */
+export const LatestContext = React.createContext([]);
+
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {}, []);
+
   return (
     <>
-      <TopAppBar/>
-      <MobileWrapper>
-        <Chips />
-        <BigStory />
-        <NewsCategory />
-        <Story />
-        <Story />
-        <Story />
-        <BigStory />
-        <NewsCategory />
-        <Story />
-        <Story />
-        <Story /> <BigStory />
-        <NewsCategory />
-        <Story />
-        <Story />
-        <Story /> <BigStory />
-        <NewsCategory />
-        <Story />
-        <Story />
-        <Story />
-        <LoadMore/>
-      </MobileWrapper>
+      <ArticlesContext.Provider value={[]}>
+        <MainArticleContext.Provider value={[]}>
+          <TrendsContext.Provider value={[]}>
+            <LatestContext.Provider value={[]}>
+
+            {/* https://youtu.be/UjjtvroahBU?list=PLC3y8-rFHvwisvxhZ135pogtX7_Oe3Q3A&t=60 */}
+              {/* break-line */}
+              <TopAppBar />
+              <MobileWrapper>
+                <Chips />
+                <BigStory />
+                <NewsCategory />
+                <Story />
+                <Story />
+                <Story />
+                <BigStory />
+                <NewsCategory />
+                <Story />
+                <Story />
+                <Story /> <BigStory />
+                <NewsCategory />
+                <Story />
+                <Story />
+                <Story /> <BigStory />
+                <NewsCategory />
+                <Story />
+                <Story />
+                <Story />
+                <LoadMore />
+              </MobileWrapper>
+            </LatestContext.Provider>
+          </TrendsContext.Provider>
+        </MainArticleContext.Provider>
+      </ArticlesContext.Provider>
     </>
   );
 }
--- src/components/util/helpers.ts
+++ src/components/util/helpers.ts
@@ -1,3 +1,9 @@
+import { string } from "prop-types";
+
+/**
+ * API TOKEN
+ */
+const TOKEN = import.meta.env.VITE_APP_TOKEN;
 /**
  *
  * @param url image URL
@@ -6,20 +12,19 @@
 export const imgData = async (url: string): Promise<Blob | null> => {
   try {
     let response: any = await fetch(url, {
-      mode: "no-cors"
+      mode: "no-cors",
     });
 
-    console.log({response});
-    
+    console.log({ response });
+
     let data = await response.blob();
 
-    console.log({data});
-    
+    console.log({ data });
 
     return data;
   } catch (error) {
     console.error(error);
-    
+
     return null;
   }
 };
@@ -43,11 +48,97 @@ export const imgDataAsync = (url: string, callback: Function) => {
 };
 
 /**
- * 
- * @param rgb 
- * @returns 
+ *
+ * @param rgb
+ * @returns
  */
-export const _rgbToHex = (rgb: Array<number>) => '#' + rgb.map(x => {
-  const hex = x.toString(16)
-  return hex.length === 1 ? '0' + hex : hex
-}).join('')
+export const _rgbToHex = (rgb: Array<number>) =>
+  "#" +
+  rgb
+    .map((x) => {
+      const hex = x.toString(16);
+      return hex.length === 1 ? "0" + hex : hex;
+    })
+    .join("");
+
+/**
+ *
+ * @param str string
+ * @returns
+ */
+export function hashCode(str: string) {
+  return Array.from(str)
+    .reduce((s, c) => (Math.imul(31, s) + c.charCodeAt(0)) | 0, 0)
+    .toString();
+}
+type ReceiveData = (args: string) => void;
+
+/**
+ * Get articles of type card
+ * @param callback
+ */
+export const fetchArticleData = (callback: ReceiveData) => {
+  ///card
+  var headers = new Headers();
+  headers.append("Authorization", `Bearer ${TOKEN}`);
+
+  fetch(
+    "https://museapi.byteestudio.com/api/graphql?query={%20filtered(first:90%20page:1%20type:%22card%22)%20{%20paginatorInfo%20{%20lastPage%20hasMorePages%20currentPage%20total%20}%20data%20{%20id%20url%20headline%20lede%20thumbnail%20category%20catLink%20images%20src_name%20src_url%20src_logo%20isVid%20vidLen%20type%20tag%20tags%20author%20authors%20date%20}%20}%20}",
+    {
+      method: "GET",
+      headers: headers,
+      redirect: "follow",
+    }
+  )
+    .then((response) => response.text())
+    .then(callback)
+    .catch((error) => console.error("Error: ", error));
+};
+/**
+ * Filter by topic
+ * @param callback
+ */
+export const fetchArticleFilteredData = (callback: Function) => {
+  /// filter by topic
+  throw new Error("Unimplemented");
+};
+/**
+ * Latest
+ * @param callback
+ */
+export const fetchLatestData = (callback: ReceiveData) => {
+  var headers = new Headers();
+  headers.append("Authorization", `Bearer ${TOKEN}`);
+
+  fetch(
+    "https://museapi.byteestudio.com/api/graphql?query={%20filtered(first:90%20page:1%20type:%22trend%22)%20{%20paginatorInfo%20{%20lastPage%20hasMorePages%20currentPage%20total%20}%20data%20{%20id%20url%20headline%20lede%20thumbnail%20category%20catLink%20images%20src_name%20src_url%20src_logo%20isVid%20vidLen%20type%20tag%20tags%20author%20authors%20date%20}%20}%20}",
+    {
+      method: "GET",
+      headers: headers,
+      redirect: "follow",
+    }
+  )
+    .then((response) => response.text())
+    .then(callback)
+    .catch((error) => console.log("error", error));
+};
+/**
+ * Hot trends
+ * @param callback
+ */
+export const fetchTrendsData = (callback: ReceiveData) => {
+  var headers = new Headers();
+  headers.append("Authorization", `Bearer ${TOKEN}`);
+
+  fetch(
+    "https://museapi.byteestudio.com/api/graphql?query={%20filtered(first:15%20page:1%20type:%22hot-trends%22){%20paginatorInfo{%20count%20currentPage%20lastPage%20total%20}%20data{%20id%20label%20url%20headline%20}%20}%20}",
+    {
+      method: "GET",
+      headers: headers,
+      redirect: "follow",
+    }
+  )
+    .then((response) => response.text())
+    .then(callback)
+    .catch((error) => console.log("error", error));
+};
