8d87e72b71684975cee06af518f448357b7916eb
Makhosandile Ndondo <makhosi@Makhosandiles-MacBook-Air.local>
2023-04-23 17:51:31 +0200

    Sun 23 Apr 2023 17:51:31 SAST - Clear react files before add flutter files

--- src/components/layout/MobileWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PropsWithChildren, ReactNode } from 'react'
-
-
-export const MobileWrapper = (props: PropsWithChildren<ReactNode>) => {
-  return (
-    <main className='mobile-wrapper main' >{props.children}</main>
-  )
-}
